26c23fb644f897f57dbaae22bdb62eed508b823b
Himanshu kumar <himanshukr1505892@gmail.com>
2025-03-19 21:13:38 +0530

    first commit

--- server/Config/Db.js
+++ server/Config/Db.js
@@ -1,18 +1,18 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-    host: '127.0.0.1',  // or 'localhost'
-    user: 'root',       // Change if you use a different username
-    password: 'himanshu@123',       // Add your MySQL Workbench password
-    database: 'testdb' // Replace with your actual database name
-});
+// const connection = mysql.createConnection({
+//     host: '127.0.0.1',  // or 'localhost'
+//     user: 'root',       // Change if you use a different username
+//     password: 'himanshu@123',       // Add your MySQL Workbench password
+//     database: 'testdb' // Replace with your actual database name
+// });
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Database connection failed:', err.stack);
-        return;
-    }
-    console.log('Connected to MySQL Workbench database');
-});
+// connection.connect((err) => {
+//     if (err) {
+//         console.error('Database connection failed:', err.stack);
+//         return;
+//     }
+//     console.log('Connected to MySQL Workbench database');
+// });
 
-module.exports = connection;
+// module.exports = connection;
